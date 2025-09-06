f80d3d13d97256f020a383cb0e7d919032c63109
danielagar320 <daniela.garcia-r@mail.escuelaing.edu.co>
2025-09-05 22:45:59 -0500

    Index adaptado para despliegue

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@ import { DatesController } from "./controllers/datesController";
 const app = express();
 const controller = new DatesController();
 
+
 app.get("/business-days", controller.getBusinessDays.bind(controller));
 
-app.listen(3000, () => {
-  console.log("Servidor escuchando en http://localhost:3000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
 
 
